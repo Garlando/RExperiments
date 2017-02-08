82356a6f5ad9bbc076e287f8f8ea77889c38fd91
Craig Garlick <c.r.garlick@gmail.com>
2017-02-08 21:42:48 +0000

    some really basic type examples

---
+++ R_basics.R
@@ -0,0 +1,12 @@
+#basic types
+new_numeric <- 1
+new_char <- 'hello world'
+new_boolean <- TRUE
+new_integer <- 1L #the L signifies an integer
+new_factor <- factor(c("factor1","factor2"))
+
+#Everything in R is a vector
+vec_a <- c("a","b","c")
+vec_i < c(1L,2L,3L)
+vec_b <- c(TRUE,FALSE)
+vec_n <- c(1.1,3.5,-8)
